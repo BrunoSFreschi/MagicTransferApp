c70e0da8683b67baf322c46e5ef162b837826996
BrunoSFreschi <bruno.freschi2@gmail.com>
2025-03-30 16:38:45 -0300

    update

--- obj/Release/net9.0/MagicTransferApp.AssemblyInfo.cs
+++ obj/Release/net9.0/MagicTransferApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicTransferApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ae06d60485ded0873c18a3be2fe629cadc9b60a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicTransferApp")]
