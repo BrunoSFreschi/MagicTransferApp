7d9144429cef349d3e6793043779c6b0288a6ed5
BrunoSFreschi <bruno.freschi2@gmail.com>
2025-03-31 01:30:10 -0300

    create

---
+++ ColumnSchema.cs
@@ -0,0 +1,10 @@
+﻿internal class ColumnSchema
+{
+    public string ColumnName { get; set; }
+    public string DataType { get; set; }
+    public int MaxLength { get; set; }
+    public int Precision { get; set; }
+    public int Scale { get; set; }
+    public bool IsNullable { get; set; }
+    public bool IsIdentity { get; set; }
+}
